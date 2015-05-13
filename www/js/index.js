39df72b8917e3ea68f06f52bc890546ed90adf31
Brad Traversy <me@bradtraversy.com>
2015-05-13 12:06:30 -0400

    commit

--- www/js/index.js
+++ www/js/index.js
@@ -1,43 +1,71 @@
-//Add event listener to run when the device is ready
+// Device Event Listener
 document.addEventListener("deviceready", onDeviceReady, false);
 
-//Device is ready
+//Device Ready
 function onDeviceReady() {
    $(document).ready(function(){
+        // Save Event Listener
        $('#saveBtn').on('click', function(){
            saveContact();
        });
+
+       getContacts();
    });
 }     
 
+// Get Contacts
+function getContacts(){
+    console.log('Getting Contacts...');
+    var options = new ContactFindOptions();
+    options.filter=""; 
+    options.multiple=true;
+    var fields = ["displayName"];
+    navigator.contacts.find(fields, onGetSuccess, onGetError, options);
+}
+
+// Contacts Fetched
+function onGetSuccess(contacts){
+    for (var i = 0; i < contacts.length; i++) {
+        $('#contactList').append('<li>'+contacts[i].displayName+'</li>');
+    }
+}
+
+// Contact Fetch Error
+function onGetError(error){
+    alert('Error: '+ error.code);
+}
+
+// Add a Contact
 function saveContact(){
     console.log('Getting Contact Info...');
-    //Create variables from form input
+    // Get Form Values
     var firstName    = $('#firstName').val();
     var lastName     = $('#lastName').val();
     var fullName     = $('#firstName').val()+' '+$('#lastName').val();
     var note         = $('#note').val();
     var emailAddress = $('#email').val();
     
-    //Create contact object
+    // Create Contact Object
     var myContact = navigator.contacts.create({"displayName" : fullName});
-    //
-    //Note Field
+    
+    // Note Field
     myContact.note = note;
     
-    //Email Field
+    // Email Field
     var emails = [];
     emails[0] = new ContactField('email', emailAddress, false); 
     myContact.emails = emails;
     
-    //Save contact info   
+    //Save Contact   
     myContact.save(onSaveSuccess, onSaveError);             
 }
 
+// Contact Added
 function onSaveSuccess(contact){
     alert('Your Contact Has Been Saved');
 }
 
+// Contact Add Error
 function onSaveError(error){
     alert('Error: '+ error.code);
 }
