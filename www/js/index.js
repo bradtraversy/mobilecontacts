57d64981cfdba7b354bd4515a0450f7b46ae7225
Brad Traversy <me@bradtraversy.com>
2015-05-13 11:14:17 -0400

    first commit

---
+++ www/js/index.js
@@ -0,0 +1,44 @@
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+        document.getElementById('saveContact').addEventListener('click', this.saveContact, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    },
+    saveContact: function(){
+        // Get Form Info
+        var full_name = document.getElementById('first_name').value+" "+document.getElementById('last_name').value;
+        var note = document.getElementById('note').value;
+
+        var myContact = navigator.contacts.create({"displayName": full_name});
+        myContact.note = note;
+
+        contact.save();
+
+        console.log('Saving Contact ' +full_name+'...');
+    }
+};
